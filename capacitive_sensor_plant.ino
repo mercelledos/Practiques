4eee933d9a8ce3475286689b2e2405f3f738d9ac
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2023-10-16 16:48:27 +0200

    Update capacitive_sensor_plant.ino

--- capacitive_sensor_plant.ino
+++ capacitive_sensor_plant.ino
@@ -1,4 +1,5 @@
-
+/* Revisió 16 octubre 2023
+Marc */
 
 // capacitive sensing includes
 #include <CapacitiveSensor.h>
@@ -6,31 +7,44 @@
 // capacitive sensing constant
 CapacitiveSensor sensor = CapacitiveSensor(4, 2);  // 1M resistor between pins 4 & 2, pin 2 is sensor pin
 
+//podeu declarar un nom per a cada led i d'aquesta manera tenir el codi més endreçat:
+const int red = 9;
+const int green = 10;
+const int blue = 11;
+
 void setup() {
   Serial.begin(115200);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
 
   pinMode (2,INPUT);
 }
 
 void loop() {
   long measurement = sensor.capacitiveSensor(30);
-
   Serial.println(measurement);
-  delay(100);
+  delay(10);
 
-  if (measurement > 100) {
-    digitalWrite(11, HIGH);
-    delay(1000);
-    digitalWrite(10, LOW);
-    delay(1000);
-    digitalWrite(9, LOW);
-    delay(1000);
+  if (measurement > 100 && measurement <= 300) {
+    digitalWrite(red, LOW);
+    digitalWrite(green, LOW);
+    digitalWrite(blue, HIGH);
+  
+  } else if (measurement > 300 && measurement <= 450) {
+    digitalWrite(red, LOW);
+    digitalWrite(green, HIGH);
+    digitalWrite(blue, LOW);
+    
+  } else if (measurement > 450) {
+    digitalWrite(red, HIGH);
+    digitalWrite(green, LOW);
+    digitalWrite(blue, LOW);
 
   } else {
-    digitalWrite(11, LOW);
-    delay(1000);
+    digitalWrite(red, LOW);
+    digitalWrite(green, LOW);
+    digitalWrite(blue, LOW);
   }
+  
 }
